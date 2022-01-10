10ca339223fa0467178bcf021aa34a44399b9db3
Sankalp Verma <sunklapverma4489@gmail.com>
2022-01-11 00:02:27 +0530

    Added google omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def new
     if logged_in?
       redirect_to root_path
-    else 
+    end 
   end
 
   #process render values
@@ -22,5 +22,23 @@ class SessionsController < ApplicationController
   def destroy # logout 
     session.delete :user_id
     redirect_to login_path
+  end
+
+  #Google omniauth
+  def omniauth
+    
+    #find user or create one
+     user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
+       u.username = request.env['omniauth.auth'][:info][:first_name] 
+       u.email = request.env['omniauth.auth'][:info][:email] 
+       u.password = SecureRandom.hex(15)
+     end
+
+     if user.valid?
+        session[:user_id] = user.id 
+        redirect_to root_path
+     else
+        redirect_to login_path
+     end
   end
 end
